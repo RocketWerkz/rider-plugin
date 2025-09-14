582b14f5fcc8a014abb2c7d41f1d2b9cfef7d6ea
brogan <brogan.king@outlook.com>
2025-09-15 11:21:17 +1200

    Add content path test

--- src/dotnet/Brutal.Plugin.Sandbox/Testing.cs
+++ src/dotnet/Brutal.Plugin.Sandbox/Testing.cs
@@ -2,9 +2,9 @@
 
 namespace RW.Brutal.Tests;
 
-public class ColorTests
+public class Testing
 {
-    public static void Test()
+    public static void TestColors()
     {
         var _color = Color.Green;
         // var _float3 = float3.Rgb(1, 0, 0);
@@ -12,5 +12,10 @@ public class ColorTests
         // var _byte3 = byte3.Rgb(0, 0, 0xFF);
         // var _byte4 = byte4.Rgba(0, 0, 255, 255);
         // var _ushort3 = ushort3.Rgb(0x0000, 0xFFFF, 0xFFFF);
+    }
+
+    public static void TestContentPath()
+    {
+        var path = "Content/Test.txt";
     }
 }
